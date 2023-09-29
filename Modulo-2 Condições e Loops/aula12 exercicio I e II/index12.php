3b2add2ef6a260a8f9628cb631563ae2bf6dc852
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-09-29 20:52:18 -0300

    exercicios

--- Modulo-2 Condições e Loops/aula12 exercicio I e II/index12.php
+++ Modulo-2 Condições e Loops/aula12 exercicio I e II/index12.php
@@ -1,6 +1,6 @@
 <?php
 $linha = '-';
-for($x=0;$x<=10;$x++){
+for($x=0;$x<10;$x++){
     echo $linha;
     for($y=0;$y<10;$y++){
         echo $linha;
---
+++ Modulo-2 Condições e Loops/aula12 exercicio I e II/index12a.php
@@ -0,0 +1,9 @@
+<?php
+$traco = '-';
+for($x=0;$x<21;$x++){
+    echo $traco;
+    for($y=0;$x>$y;$y++){
+        echo $traco;
+    }
+    echo "<br>"; 
+}

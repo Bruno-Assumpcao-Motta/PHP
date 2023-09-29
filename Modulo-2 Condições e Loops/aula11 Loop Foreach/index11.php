a7c2167977d25a25ca5ecf0eef9dbe1cdde143ee
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-09-29 19:28:59 -0300

    Update index11.php

--- Modulo-2 Condições e Loops/aula11 Loop Foreach/index11.php
+++ Modulo-2 Condições e Loops/aula11 Loop Foreach/index11.php
@@ -6,9 +6,20 @@ $ingredientes = [
     'leite',
     'fermento em pó'
 ];
-
+/*
 echo '<h2>Ingredientes</h2>';
 
 foreach($ingredientes as $chave => $valor){
     echo "Item ".($chave + 1).": ".$valor."<br/>";
 }
+*/
+
+// para fazer uma lista desordenada com os valores
+
+echo '<h2>Ingredientes</h2>';
+
+echo '<ul>';
+foreach($ingredientes as $valor){
+    echo '<li>'.$valor.'</li>';
+}
+echo '</ul>';

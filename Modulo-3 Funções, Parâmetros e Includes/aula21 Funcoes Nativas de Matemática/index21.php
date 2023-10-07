22bb7a2cff2ddfedae29d86937d713a84f58ce4a
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-10-07 17:11:04 -0300

    Update index21.php

--- Modulo-3 Funções, Parâmetros e Includes/aula21 Funcoes Nativas de Matemática/index21.php
+++ Modulo-3 Funções, Parâmetros e Includes/aula21 Funcoes Nativas de Matemática/index21.php
@@ -1,4 +1,12 @@
 <?php
+// abs Transforma em numero positivo
+// floor arredonda um valor sempre para baixo
+// ceil arredonda um valor sempre para cima
+// round arredonda para o maior ou menor
+// rand gera um número aleatório a partir de um min e max
+// max para saber o maior valor em uma lista 
+// mim para saber o menor valor em uma lista
+
 $lista = [1, 4, 8, 9];
 
-echo max($lista);
+echo min($lista);
